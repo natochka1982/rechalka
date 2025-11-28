d14e102787e71f45853c37effa59271a227c16eb
natochka1982 <134779149+natochka1982@users.noreply.github.com>
2025-11-28 11:21:59 +0600

    Add files via upload

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ load_dotenv()
 class Config:
     BOT_TOKEN = os.getenv('7743746871:AAGy0h63RtGbrf9JnTk9ymFm_PP7HjRKvSQ')
     GIGACHAT_CLIENT_ID = os.getenv('GIGACHAT_CLIENT_ID')
-    GIGACHAT_SECRET = os.getenv('GIGACHAT_SECRET')
+    GIGACHAT_SECRET = os.getenv('MDE5YWJjNDUtZTAzMi03NmJmLWE5YmYtNWRlMDlhY2QxYzRlOmRjOTk0NmUyLTNjYmUtNDdlYy04OGE4LTFhMzc3OGQxZTg4Mw==')
     
     # Проверяем, что токены загружены
     if not BOT_TOKEN:
